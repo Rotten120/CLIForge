e5613e67f6aae87b8b28dcb048d0218910d1e714
Rotten120 <thenanokid@gmail.com>
2025-10-01 15:09:02 +0800

    changed FlagBool to FlagToggle and FlagList to FlagValue; FlagToggle now strictly does not accept any argument (the True/False ones), it changes its value opposite of the default when called

--- src/cmds/flag.py
+++ src/cmds/flag.py
@@ -5,47 +5,37 @@ class Flag:
 
     def __check_argcnt(self, flag_name: str, val_arglen: int) -> None:
         argcount = self.argcnt
-        if argcount >= 0:
-            if val_arglen != argcount:
-                raise TypeError(f"{flag_name} takes {argcount} positional arguments but {val_arglen} was given")
-        else:
-            if val_arglen == 0:
-                raise TypeError(f"{flag_name} takes at least 1 positional arguments but 0 was givne")
-
+        if argcount >= 0 and val_arglen != argcount:
+            raise TypeError(f"{flag_name} takes {argcount} positional arguments but {val_arglen} was given")
+        if argcount < 0 and val_arglen <= 0:
+            raise TypeError(f"{flag_name} takes at least 1 positional arguments but 0 was given")
+        
     def update_flag(self, flag_name: str, value: list[str] == []) -> None:
         self.__check_argcnt(flag_name, len(value))
         self.value = value
 
-# FlagBool ignores __check_argcnt since it only needs 0/1 args
-class FlagBool(Flag):
+    def print(self):
+        print(f"value={self.value} | argcnt={self.argcnt}")
+
+class FlagToggle(Flag):
     def __init__(self, default: bool):
-        super().__init__(default, 1)
+        super().__init__(default, 0)
+        self.toggled_val = not default
 
     def __bool__(self):
         return self.value
 
     def update_flag(self, flag_name: str, value: list[str] == []) -> None:
         val_arglen = len(value)
-        if val_arglen == 0:
-            self.value = not self.value
-        elif val_arglen == 1:
-            self.value = self.__str_to_bool(value[0])
-        else:
-            raise TypeError(f"{flag_name} takes 0 or 1 positional arguments but {val_arglen} was given")
+        if val_arglen != 0:
+            err_msg = f"{flag_name} takes 0 positional arguments but {val_arglen} was given"
+            raise TypeError(err_msg)
+        self.value = self.toggled_val
 
     def print(self):
-        print(self.value, self.argcnt)
+        print(f"value={self.value} | toggled value={self.toggled_val} | argcnt={self.argcnt}")
 
-    @staticmethod
-    def __str_to_bool(value: str) -> bool:
-        if value == "True" or value == "true":
-            return True
-        elif value == "False" or value == "false":
-            return False
-        else:
-            raise Exception(f"flag takes 'True' or 'False' argument but {value} was given")
-
-class FlagList(Flag):
+class FlagValue(Flag):
     def __init__(self, default: list[str], argcnt: int):
         super().__init__(default, argcnt)
 
